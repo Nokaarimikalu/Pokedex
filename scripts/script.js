36a812f6af0f80a38f9088eeba81ca9a7234c498
Pascal Fliednr <fliednerpascal@gmail.com>
2025-05-09 23:27:06 +0200

    v3

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 let allPokemonData = [];
-let offset = 20;
 
 async function init() {
     await getData();
     allDataForTemplate();
 }
 
+console.log(allPokemonData);
+
 async function getData() {
-    let url = `https://pokeapi.co/api/v2/pokemon?limit=${offset}`;
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=40&offset=${allPokemonData.length}`;
     let response = await fetch(url);
     let currentRequest = await response.json();
     for (let i = 0; i < currentRequest.results.length; i++) {
@@ -22,18 +23,104 @@ function allDataForTemplate() {
     let mainArea = document.querySelector(`.test`);
     for (let i = 0; i < allPokemonData.length; i++) {
         const element = allPokemonData[i];
-        const index = i;
-        mainArea.innerHTML += templatePokemonOverlay(element, index);
+        mainArea.innerHTML += templatePokemonOverlay(element);
     }
 }
 
-console.log(allPokemonData);
-
-function showPokemonDetails(i) {
-    document.getElementById("popUpPokemon").classList.remove("d_hidden");
-    console.log(`${i}`);
+function showPokemonDetails(indexOfPokemon) {
+    let pokemon = allPokemonData[indexOfPokemon];
+    let popUpArea = document.querySelector(`#popUpSection`);
+    popUpArea.innerHTML = "";
+    popUpArea.innerHTML = templatePopUp(pokemon, indexOfPokemon);
+    pokemonStats(pokemon);
+    pokemonAbility(pokemon);
+    pokemonType(pokemon);
 }
 
 function closePopUp() {
     document.getElementById("popUpPokemon").classList.add("d_hidden");
+}
+
+function pokemonStats(pokemon) {
+    let pokemonStats = document.querySelector(`#pkmStats`);
+    for (let i = 0; i < pokemon.stats.length; i++) {
+        const attribute = pokemon.stats[i];
+        pokemonStats.innerHTML += /*html*/ `
+            <h2><b>${attribute.stat.name.charAt(0).toUpperCase() + attribute.stat.name.slice(1)}:</b>${attribute.base_stat}</h2>
+        `;
+    }
+}
+
+function pokemonAbility(pokemon) {
+    let pokemonAbility = document.querySelector(`.ability`);
+    for (let i = 0; i < pokemon.abilities.length; i++) {
+        const ability = pokemon.abilities[i];
+        const name = ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1);
+
+        if (ability.is_hidden) {
+            pokemonAbility.innerHTML += `<p><b>Hidden Ability:</b> ${name}</p>`;
+        } else {
+            pokemonAbility.innerHTML += `<p><b>Ability:</b> ${name}</p>`;
+        }
+    }
+}
+
+function pokemonType(pokemon) {
+    let pokemonType = document.querySelector(`.elemente`);
+    for (let i = 0; i < pokemon.types.length; i++) {
+        const types = pokemon.types[i];
+        const type = types.type.name.charAt(0).toUpperCase() + types.type.name.slice(1);
+        pokemonType.innerHTML += /*html*/ `
+            <div class="type type-${types.type.name}">${type}</div>
+        `;
+    }
+}
+
+function nextPokemon(currentIndex) {
+    let newIndex = currentIndex + 1;
+    if (newIndex < allPokemonData.length) {
+        showPokemonDetails(newIndex);
+    }
+}
+
+function prevPokemon(currentIndex) {
+    let newIndex = currentIndex - 1;
+    if (newIndex >= 0) {
+        showPokemonDetails(newIndex);
+    }
+}
+
+async function loadMorePokemon(button) {
+    const text = button.querySelector(".btn-text");
+    const loading = button.querySelector(".pokeball-loader");
+    text.textContent = "Loading...";
+    loading.style.display = "inline-block";
+    button.disabled = true;
+    const oldLength = allPokemonData.length;
+    try {
+        await getData();
+        let m = document.querySelector(".test");
+        for (let i = oldLength; i < allPokemonData.length; i++) m.innerHTML += templatePokemonOverlay(allPokemonData[i]);
+    } catch (error) {
+        console.error("Fehler:", error);
+    }
+    text.textContent = "Next";
+    loading.style.display = "none";
+    button.disabled = false;
+}
+
+function searchPokemon(info) {
+    let mainArea = document.querySelector(`.test`);
+    mainArea.innerHTML = "";
+    if (info.length === 0) {
+        allDataForTemplate();
+    } else if (info.length < 3) {
+        return;
+    } else {
+        const filteredPokemon = allPokemonData.filter((pokemon) => pokemon.name.toLowerCase().includes(info.toLowerCase()));
+
+        filteredPokemon.forEach((pokemon) => {
+            mainArea.innerHTML += templatePokemonOverlay(pokemon);
+        });
+    }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,10 +1,32 @@
-function templatePokemonOverlay(pokemon, index) {
+function templatePokemonOverlay(pokemon) {
     return /*html*/ `
-            <div class="pokemon" onclick="showPokemonDetails(${index})">
+            <div class="pokemon" onclick="showPokemonDetails(${pokemon.id - 1})">
                 <div class="pokeImage"><img src="${pokemon.sprites.front_default}" alt="" /></div>
                 <div class="pokeName">
                     <h2>#${pokemon.id}</h2>
                     <h1>${pokemon.name}</h1>
+                </div>
+            </div>
+    `;
+}
+
+function templatePopUp(currentPokemon, index) {
+    return /*html*/ `
+            <div id="popUpPokemon" onclick="closePopUp()">
+                <div class="popUpInfoPokemon" onclick="event.stopPropagation()">
+                    <div class="popUpImage">
+                        <div class="elemente">
+                        </div>
+                        <img src="${currentPokemon.sprites.front_default}" alt="" />
+                        <div class="ability">
+                        </div>
+                        <div class="buttonsForPopUp">
+                            <button onclick="prevPokemon(${index})">< Previous</button>
+                            <button onclick="nextPokemon(${index})">Next ></button>
+                        </div>
+                    </div>
+                    <div id="pkmStats" class="statsOfthePokemon">
+                    </div>
                 </div>
             </div>
     `;
